4b042b53504469116a68424c0c57004857185219
Andrei <andrei@studios-pro.com>
2026-02-23 20:18:43 -0500

    SECURITY: Remove tracked .env and modularize Firebase keys to unblock Netlify builds

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBNB1Whl4DCQPLGiCmAOpW7yXK1uGZXc9c",
-    authDomain: "studios-pro.firebaseapp.com",
-    projectId: "studios-pro",
-    storageBucket: "studios-pro.firebasestorage.app",
-    messagingSenderId: "337301506363",
-    appId: "1:337301506363:web:68b84724390b830bbf4f1e",
-    measurementId: "G-PG01CWHZDF"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
