ab3f9cd82a612e4bc9e5da5673fd46321b954061
Andrei <andrei@studios-pro.com>
2026-07-21 01:17:54 -0400

    fix: remove ALL service workers completely - unregister everything to fix AR Viewer QR code loading on mobile

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,9 @@
-// This service worker auto-unregisters itself to fix AR Viewer on mobile.
-// The community SW is now at /community/sw.js
+// Cleanup service worker - unregisters itself immediately
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
       .then(() => self.registration.unregister())
-      .then(() => self.clients.matchAll())
-      .then(clients => clients.forEach(c => c.navigate(c.url)))
   );
 });
